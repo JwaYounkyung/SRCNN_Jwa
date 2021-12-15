a8b86ae04db4a0ce396536f9c1d3650aaa460b23
JwaYounkyung <50140321+JwaYounkyung@users.noreply.github.com>
2021-12-15 10:29:20 +0900

    Update SRCNN_classes.py

--- SRCNN_classes.py
+++ SRCNN_classes.py
@@ -4,7 +4,7 @@ import torchvision
 
 ### SRCNN model
 class SRCNN(nn.Module):
-    def __init__(self, num_channels=1, kernel_size=[9,1,5]): # in_channels : RGB or YCbCr
+    def __init__(self, num_channels=1, kernel_size=[9,1,5]): # num_channels : RGB or YCbCr
         super(SRCNN, self).__init__()
         self.layer1 = nn.Sequential(
             nn.Conv2d(in_channels=num_channels, out_channels=64, kernel_size=kernel_size[0], stride=1, padding=0),
